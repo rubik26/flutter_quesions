871a8d653a1cd62ff4835e0f7bb7818d4830ccc7
Rubo <80164820+rubik26@users.noreply.github.com>
2024-11-25 18:29:36 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quesions/background_color.dart';
 import 'package:flutter_quesions/exam.dart';
+import 'package:flutter_quesions/result.dart';
 
 void main() {
   const colorList = [Colors.purple, Colors.purpleAccent];
   runApp(
     MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/', routes: {
       '/': (context) => const GradientColor(colorList: colorList),
-      '/exam': (context) => const Quesions()
+      '/exam': (context) => const Questions(),
+      '/result': (context) => const ResultsScreen(),
     }),
   );
 }
